6405edbf1c1d66d753c96f7eac7fe708fcc394a2
bkchu <brandon.k.chung@gmail.com>
2018-03-20 14:56:36 -0500

    finished stage4

--- stage-4/src/index.js
+++ stage-4/src/index.js
@@ -1,15 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { Provider } from "react-redux";
+import { HashRouter as Router } from "react-router-dom";
+import store from "./redux/store";
 
 ReactDOM.render(
-
-    <Provider store={store}>
-        <App />
-    </Provider>
-    , document.getElementById('root'));
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
